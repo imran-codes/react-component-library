557878166c5a8f66b0cd917d2c08ecab4f7478e9
imranh481 <imranh160@gmail.com>
2024-02-26 08:59:16 +0000

    chore: made a small change to styling

--- src/components/Page/styled.ts
+++ src/components/Page/styled.ts
@@ -5,6 +5,6 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100vh;
+  height: 90vh;
   background-color: #f5f5f5;
 `;
